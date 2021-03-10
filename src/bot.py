71b495584a2740c37dbde6b94be61e65376c5321
mirovoy <cool-dimon57500@yandex.ru>
2021-03-11 01:08:12 +0300

    moved misc commands to new cog

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,6 @@
 # bot.py
 import os
-import random
-
-from discord import guild
+from pathlib import Path
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,21 +9,48 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot("gw!")
+class gerawowBot(commands.Bot):
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} ready!')
-    bot.load_extension('cogs.music')
+    def __init__(self):
+        self._cogs = [p.stem for p in Path(".").glob("./src/cogs/*.py")]
+        super().__init__(command_prefix = self.prefix, case_insensetive = True)
+    
+    def setup(self):
+        print("Booting up...")
+
+        for cog in self._cogs:
+            self.load_extension(f'cogs.{cog}')
+            print(f'Loaded cog: {cog}!')
+
+        print("Setup complete!")
+    
+    def run(self):
+        super().run(TOKEN, reconnect = True)
+
+    async def on_connect(self):
+        self.setup()
+        print(f'{self.user} connected! Latency :{self.latency*1000:,.1f} ms')
+
+    async def on_ready(self):
+        print(f'{self.user} ready!')
+        self.client_id = (await self.application_info()).id
+
+    async def on_disconnect(self):
+        print(f'{self.user} disconnected!')
+    
+    async def prefix(self,bot,message):
+        return commands.when_mentioned_or("gw!")(bot,message)
+
+    async def process_commands(self, message):
+        ctx = await self.get_context(message, cls=commands.Context)
+        if ctx.command is not None:
+            await self.invoke(ctx)
+        
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.process_commands(message)
+    
 
 
-@bot.command(name='gerawow',help='gerawow')
-async def gerawow(ctx):
-    await ctx.send("testmessage <:pepopeek:574292228198105106>")
-
-@bot.command(name='roll',help='Returns a random number')
-async def roll(ctx, num : int):
-    await ctx.send(random.choice(range(1,num+1)))
-
-
-bot.run(TOKEN)
+bot = gerawowBot()
+bot.run()
